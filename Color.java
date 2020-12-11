7af727e4ffe317defbbd9e739035ddf486e46503
priyamengji99 <75771837+priyamengji99@users.noreply.github.com>
2020-12-11 09:54:32 +0530

    Switch Statemet

---
+++ Color.java
@@ -0,0 +1,27 @@
+
+public class Color {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String color="white";
+		switch(color)
+		{
+		case "voilet":System.out.println("voilet");
+					  break;
+		case "blue":System.out.println("blue");
+		            break;
+		case "Indigo":System.out.println("Indigo");
+					  break;
+		case "grey":System.out.println("grey");
+					break;
+		case "orange":System.out.println("orage");
+					  break;
+		case "Red":System.out.println("red");
+				   break;
+		case "yellow":System.out.println("yellow");
+				   break;
+		default:System.out.println("not a valid color");
+		}
+	}
+
+}
