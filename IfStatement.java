0151fc6030d7b70cbcd0a1046fa5b3401b98cb3d
priyamengji99 <75771837+priyamengji99@users.noreply.github.com>
2020-12-11 10:06:49 +0530

    If Statement

---
+++ IfStatement.java
@@ -0,0 +1,15 @@
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num=12345,count=0;
+		while(num!=0)
+		{
+			num=num/10;
+			count++;
+		}
+		System.out.println("the no of digits are"+count);
+	}
+}
