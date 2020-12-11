31c7b8d83c03130987704794cafe43c03c5f7ff7
priyamengji99 <75771837+priyamengji99@users.noreply.github.com>
2020-12-11 22:11:55 +0530

    for loop

---
+++ NaturalNumbers.java
@@ -0,0 +1,12 @@
+
+public class NaturalNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=25,i;
+		for(i=0;i<=n;i++)
+		{
+			System.out.println(i+" ");
+		}
+	}
+}
