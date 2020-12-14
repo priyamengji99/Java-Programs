c4bee97dda668f4e756591f4f7b9ebeb11e9e67e
priyamengji99 <75771837+priyamengji99@users.noreply.github.com>
2020-12-14 09:11:59 +0530

    Defaullt Modifier

---
+++ Pen.java
@@ -0,0 +1,17 @@
+package com.xworkz.accessmodifier.defaultmodifier;
+
+public class Pen{
+	
+	String name;
+	int price;
+	Pen(String iname,int iprice)
+	{
+		name=iname;
+		price=iprice;
+	}
+	void Writing()
+	{
+		System.out.println("used for writing");
+	}
+
+}
